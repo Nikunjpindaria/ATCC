62f9c29c83ebdc56af2f7357b94ae8a2216c78f2
Nikunjpindaria <editsytmanage@gmail.com>
2026-05-08 19:09:15 +0530

    Add files via upload

---
+++ Append2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    FILE *fp1,*fp2;
+    fp1 = fopen("./n.txt","r");
+    fp2 = fopen("./p.txt","a");
+    char c;
+    while (fscanf(fp1, "%c", &c)!=EOF) {
+        fprintf(fp2, "%c",c);
+    }
+    fclose(fp1);
+    fclose(fp2);
+}
